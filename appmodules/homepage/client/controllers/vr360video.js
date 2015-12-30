e05d739421fae215c51982fe51a3545e629060a5
unknown <elia@LT-ELIA_X230.RF-Dynamics.local>
2015-12-30 14:14:10 +0200

    add video player for 360

--- appmodules/homepage/client/controllers/vr360video.js
+++ appmodules/homepage/client/controllers/vr360video.js
@@ -1,10 +1,28 @@
 'use strict';
 
 app.controller('vr360videoController', ['$scope', '$state', 'authToken',
-  function ($scope, $state, authToken)
-  {
+  function ($scope, $state, authToken) {
 
 
+    $(document).ready(function () {
+      $('.valiantPhoto').Valiant360();
+    });
+
+
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-608903-14', 'flimshaw.net');
+    ga('send', 'pageview');
 
 
   }
